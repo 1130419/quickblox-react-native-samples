76820a7968ff4ee1d2f70e3c5d4bb8640c0556dc
Paulo Francisco Ferreira Pereira <paulopereira@MBP-de-Paulo.lan>
2019-12-16 23:36:26 +0000

    Fix

--- chat-sample/src/QBConfig.js
+++ chat-sample/src/QBConfig.js
@@ -1,8 +1,8 @@
 export default {
-  appId: '',
-  authKey: '',
-  authSecret: '',
-  accountKey: '',
-  apiEndpoint: '',
-  chatEndpoint: '',
-}
+    appId: "79581",
+    authKey: "6AyupOcBPZzLKtg",
+    authSecret: "sTfxGSLz3XWPn2Q",
+    accountKey: "ZKCjr24CVz1d7ei4sKeG",
+    apiEndpoint: "",
+    chatEndpoint: ""
+};
