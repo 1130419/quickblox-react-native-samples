485fba57c98beb2072505b37f18293e3dc29680d
Paulo Francisco Ferreira Pereira <paulopereira@MacBook-Pro-de-Paulo.local>
2019-12-18 08:50:38 +0000

    Update QBConfig

--- chat-sample/src/QBConfig.js
+++ chat-sample/src/QBConfig.js
@@ -1,7 +1,7 @@
 export default {
-    appId: "79581",
-    authKey: "6AyupOcBPZzLKtg",
-    authSecret: "sTfxGSLz3XWPn2Q",
+    appId: "79628",
+    authKey: "nVCGmKmQy5zCLud",
+    authSecret: "TQSpZ-KAsOGzXKW",
     accountKey: "ZKCjr24CVz1d7ei4sKeG",
     apiEndpoint: "",
     chatEndpoint: ""
